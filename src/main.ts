8367bbb9402637ede8de2e2d1a5b968a35e7d336
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-03-18 18:26:05 +0300

    feat: Добавлен Rate Limiter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
+import { RateLimiter } from './middleware/rate-limiter';
 
 const server = express();
 
 const port = 2000;
+
+server.use(RateLimiter);
 server.get('/task/:id', (request: Request, res: Response) => {
   const id = request.params.id;
   res.json({ message: `Вы запросили id=${id}` });
