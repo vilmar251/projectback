5acbda6bbc262a999616539c8d0e00fb035a2599
Misha Lel <lelmisa566@gmail.com>
2025-03-25 18:00:38 +0300

    fix: Удалено 4 эндпоинта

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -2,25 +2,6 @@ import express, { Request, Response } from 'express';
 
 const userController = express.Router();
 
-userController.get('/profile', (req: Request, res: Response) => {
-  res.status(501).json({ message: 'Not Implemented' });
-});
-
-// Логин пользователя
-userController.post('/login', (req: Request, res: Response) => {
-  res.status(501).json({ message: 'Not Implemented' });
-});
-
-// Регистрация нового пользователя
-userController.post('/registration', (req: Request, res: Response) => {
-  res.status(501).json({ message: 'Not Implemented' });
-});
-
-// Обновление данных пользователя
-userController.put('/profile', (req: Request, res: Response) => {
-  res.status(501).json({ message: 'Not Implemented' });
-});
-
 // Удаление пользователя
 userController.delete('/profile', (req: Request, res: Response) => {
   res.status(501).json({ message: 'Not Implemented' });
