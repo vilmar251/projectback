60c099414ed93e4baeeec85fe880e97f9837c283
Misha Lel <lelmisa566@gmail.com>
2025-04-07 17:42:19 +0300

    feat: Создал dto для регистрации

---
+++ src/modules/task/dto/registration.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class RegistrationDto {
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+}
+su;
