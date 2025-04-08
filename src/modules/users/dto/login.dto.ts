5d0088d77ac0da31ce0b42c43f50c6e8d2ef005e
Misha Lel <lelmisa566@gmail.com>
2025-04-08 17:08:16 +0300

    feat: Добавлена авторизация пользователя

---
+++ src/modules/users/dto/login.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  email: string;
+
+  @IsString()
+  @MinLength(5, { message: 'Password must be at least 6 characters long' })
+  password: string;
+}
