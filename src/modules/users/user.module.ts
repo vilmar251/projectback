9fdd218ea1b63a2cc66d39d4f01192bddff870f9
Misha Lel <lelmisa566@gmail.com>
2025-05-13 19:00:51 +0300

    fix: Исправление ошибок 3

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -2,4 +2,4 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 const service = new UserService();
-export const userController = new UserController(service);
+export const userController = new UserController();
