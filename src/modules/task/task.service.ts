069d5cd5b547604800f478d8bb340f99b216cd59
Misha Lel <lelmisa566@gmail.com>
2025-05-22 18:42:37 +0300

    feat: Добавил подгрузку данных автора в API задач

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,16 +1,29 @@
 import { TaskEntity } from '../../database/entities/task.entity';
+import { UserEntity } from '../../database/entities/user.entity';
 import logger from '../../logger/pino.logger';
 import { Task } from './task.types';
 
 export default class TaskService {
   async findAll(): Promise<TaskEntity[]> {
     logger.info('Чтение всего списка задач');
-    return TaskEntity.findAll();
+    return TaskEntity.findAll({
+      include: [{
+        model: UserEntity,
+        as: 'author',
+        attributes: ['id', 'email']
+      }]
+    });
   }
 
   async findById(id: number): Promise<TaskEntity | null> {
     logger.info(`Чтение задачи по id=${id}`);
-    const task = await TaskEntity.findByPk(id);
+    const task = await TaskEntity.findByPk(id, {
+      include: [{
+        model: UserEntity,
+        as: 'author',
+        attributes: ['id', 'email']
+      }]
+    });
     if (!task) {
       throw Error('Task not found');
     }
