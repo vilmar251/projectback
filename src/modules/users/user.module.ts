2e692b88236d508fb84be21a9a4f238446093d9e
Misha Lel <lelmisa566@gmail.com>
2025-05-29 17:32:52 +0300

    fix: Исправлена ошибка с билдом

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,5 +1,7 @@
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import UserController from './user.controller';
+import UserService from './user.service';
 
+// Этот файл больше не нужен, так как мы используем Inversify для внедрения зависимостей
+// Оставляем для обратной совместимости
 const service = new UserService();
-export const userController = new UserController();
+export const userController = new UserController(service);
